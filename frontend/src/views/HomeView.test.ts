6f43af9baf82dd2139d3cb41f73980baeabf40a1
Raka Bintang Pratama <68149490+Anemays@users.noreply.github.com>
2025-07-31 15:45:37 +0700

    Remove unnecessary frontend files

--- frontend/src/views/HomeView.test.ts
+++ frontend/src/views/HomeView.test.ts
@@ -1,20 +1,14 @@
 import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import HomeView from './HomeView.vue';
-import TheWelcome from '../components/TheWelcome.vue';
 
 describe('HomeView', () => {
   it('renders properly', () => {
     const wrapper = mount(HomeView, {
-      global: {
-        stubs: {
-          TheWelcome: true,
-        },
-      },
+      global: {},
     });
 
     expect(wrapper.exists()).toBe(true);
     expect(wrapper.text()).toContain('Tailwind Aktif ✔');
-    expect(wrapper.findComponent(TheWelcome).exists()).toBe(true);
   });
 });
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue';
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
     <div class="p-4 bg-blue-200 text-center rounded-lg shadow">
       Tailwind Aktif ✔
     </div>
-
-    <TheWelcome />
   </main>
 </template>
